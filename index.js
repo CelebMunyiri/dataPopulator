3dad225c5ec5846456d362101adf01a5e668e67c
David Munyiri <davidmumyiri2019@gmail.com>
2024-03-22 07:35:53 +0300

    created the file and added functionality of reading a pdf file

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs=require('fs');
+const pdfParse=require('pdf-parse');
+
+const pdfFile=fs.readFileSync('./UserLocation setup.pdf');
+
+//getting the pdf details
+
+pdfParse(pdfFile).then(function(data){
+    console.log(data.numpages);
+    console.log(data.text);
+})
